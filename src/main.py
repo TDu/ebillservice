beaa5bcf5fa496233a406a11cd07cbf44c9065fd
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2021-11-22 14:24:53 +0100

    Zeep is setup

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
 from paynet import paynet
 
-p = paynet.Paynet()
+p = paynet.Paynet("https://ebill-ki.postfinance.ch/B2BService/B2BService.svc", True)
 p.ping()
--- src/paynet/paynet.py
+++ src/paynet/paynet.py
@@ -2,12 +2,12 @@ import os
 import requests
 import zeep
 
-SSL_CERTIF = os.path.join(os.path.dirname(__file__), "certificates", "ebill-ki.postfinance.ch.cer")
-WSDL_DOC = os.path.join(os.path.dirname(__file__), "certificates", "ebill-ki.postfinance.ch.cer")
+SSL_CERTIF = os.path.join(os.path.dirname(__file__), "certificates", "SwissSign_Gold_CA-G2.cer")
+WSDL_DOC = os.path.join(os.path.dirname(__file__), "wsdl", "b2bservice.wsdl")
 
 
 class Paynet:
-    def __init__(self):
+    def __init__(self, url, test_service):
         self.use_test_service = True
         settings = zeep.Settings(xml_huge_tree=True)
         session = requests.Session()
